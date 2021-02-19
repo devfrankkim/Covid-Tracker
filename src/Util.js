c75bf1e8a7de3c4a424ae321f55ae5b35c6d9159
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2021-02-19 13:55:50 -0500

    adding util functions

---
+++ src/Util.js
@@ -0,0 +1,11 @@
+export const sortData = (data) => {
+  const sortedData = [...data];
+  sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
+  return sortedData;
+};
+
+// export const sortData = (data) => {
+//   const sortedData = data;
+//   sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
+//   return sortedData;
+// };

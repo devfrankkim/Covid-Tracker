5337f2f82fa3b3ad140ed7105c1ce1f43e5f8198
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2021-02-20 19:57:31 -0500

    Added: Info Box

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -8,9 +8,9 @@ function InfoBox({ title, cases, total }) {
         <Typography className="infoBox__title" color="primary">
           {title}
         </Typography>
-        <h2 className="infoBox__cases">{cases} Cases</h2>
+        <h2 className="infoBox__cases">TODAY: {cases} </h2>
         <Typography className="infoBox__total" color="textSecondary">
-          {total} Total
+          Total: {total}
         </Typography>
       </CardContent>
     </Card>

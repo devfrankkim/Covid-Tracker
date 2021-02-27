b20c002b332ee27db092ecc978cb93c0e3b2143b
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2021-02-27 18:27:23 -0500

    working on Map

--- src/Map.js
+++ src/Map.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./Map.css";
 import { MapContainer, TileLayer } from "react-leaflet";
+import { showDataOnMap } from "./Util";
 
-import "./Map.css";
-// import { showDataOnMap } from "./util";
-
-function Map({ center, zoom }) {
+function Map({ center, zoom, countries, casesType }) {
   return (
     <div className="map">
       {" "}
-      <MapContainer center={center} zoom={zoom} scrollWheelZoom={false}>
+      <MapContainer center={center} zoom={zoom}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {showDataOnMap(countries, casesType)}
       </MapContainer>
     </div>
   );
+  ``;
 }
 
 export default Map;
